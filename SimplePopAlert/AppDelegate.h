f1ec2d037c878964e541b5f5328b6f793820855c
Tolecen <taox.l@163.com>
2015-03-17 18:27:44 +0800

    Initial Commit

---
+++ SimplePopAlert/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimplePopAlert
+//
+//  Created by Tolecen on 15/3/17.
+//  Copyright (c) 2015年 Tolecen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
